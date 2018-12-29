fa4c498c8d1ee282468e1beb86dc27ef966900ee
yohan <783b8c87@scimetis.net>
2018-12-29 03:20:07 +0100

    Fixed crontab owner.

--- start_or_update.sh
+++ start_or_update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-unset VERSION; VERSION=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-reverse-proxy.git| head -1 | cut -f 1|cut -c -10) sudo -E bash -c 'docker-compose up -d'
+sudo chown root. crontab
+# --force-recreate is used to recreate container when crontab file has changed
+unset VERSION; VERSION=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-reverse-proxy.git| head -1 | cut -f 1|cut -c -10) sudo -E bash -c 'docker-compose up -d --force-recreate'
 
