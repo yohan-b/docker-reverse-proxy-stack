9b7ed2f5e0429bf4952e5ac00186de1e813e5eba
yohan <783b8c87@scimetis.net>
2019-02-03 22:00:37 +0100

    Added pulling cron image from git.

--- start_or_update.sh
+++ start_or_update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo chown root. crontab
 # --force-recreate is used to recreate container when crontab file has changed
-unset VERSION; VERSION=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-reverse-proxy.git| head -1 | cut -f 1|cut -c -10) sudo -E bash -c 'docker-compose up -d --force-recreate'
+unset VERSION_PROXY VERSION_CRON
+VERSION_PROXY=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-reverse-proxy.git| head -1 | cut -f 1|cut -c -10) \
+VERSION_CRON=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-cron.git| head -1 | cut -f 1|cut -c -10) \
+ sudo -E bash -c 'docker-compose up -d --force-recreate'
 
